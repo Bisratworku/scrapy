31d330c6ef711c51db8e891f388a37d7d72d58b5
Bisratworku <101055036+Bisratworku@users.noreply.github.com>
2024-03-21 04:13:31 -0700

    Revert "deleted img_shape parameter"
    This reverts commit 687e0de963faafe48072cb822737b305b8d314eb.

--- scrapy/nn.py
+++ scrapy/nn.py
@@ -40,7 +40,15 @@ class Layer_Dense:
         self.dinputs = np.dot(dvalues, self.weights.T)
 
 class conv2D:
-    def __init__(self,* , kernel : tuple, padding : int = 0, stride :int = 1):
+    def __init__(self, img_shape : tuple,* , kernel : tuple, padding : int = 0, stride :int = 1):
+        if len(img_shape) == 4 :
+            x_out = int(np.ceil((((img_shape[2] - kernel[1]) + 2 * padding)/stride) + 1))
+            y_out = int(np.ceil((((img_shape[3] - kernel[2]) + 2 * padding)/stride) + 1))
+            self.img_shape = np.array([kernel[0], img_shape[0] , x_out, y_out])
+        elif len(img_shape) == 3:
+            x_out = int(np.ceil((((img_shape[1] - kernel[1]) + 2 * padding)/stride) + 1))
+            y_out = int(np.ceil((((img_shape[2] - kernel[2]) + 2 * padding)/stride) + 1))
+            self.img_shape = np.array([kernel[0], img_shape[0], x_out, y_out])
         self.weights = np.random.randn(*kernel)
         self.padding = padding
         self.stride = stride
@@ -78,6 +86,13 @@ class conv2D:
         except:
             return "the image can no longer be convolved"
 class MaxPool_2D:
+    def __init__(self , img_shape: tuple):
+        try:
+            x_out = int(np.ceil(((img_shape[2] - 2)/2) + 1))
+            y_out = int(np.ceil(((img_shape[3] - 2)/2) + 1))
+            self.img_shape = np.array([img_shape[0], img_shape[1], x_out, y_out])
+        except :
+            print("Please insert a 4D shaped array")
     def pool(self, img):
         x_out = int(np.ceil(((img.shape[0] - 2)/2) + 1))
         y_out = int(np.ceil(((img.shape[1] - 2)/2) + 1))
@@ -428,10 +443,12 @@ class model:
 
 
 
-Model = model()
 
-Model.add(conv2D(kernel = (12,3,3), padding=  1, stride= 2))
-print(Model.layers[0].deapth)
+
+
+
+
+
 
 
 
