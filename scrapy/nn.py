02adae1d608c5e35d24bc591e3ee2666655dfe65
Bisratworku <101055036+Bisratworku@users.noreply.github.com>
2025-02-20 09:55:54 -0800

    added mean squaired error

--- scrapy/nn.py
+++ scrapy/nn.py
@@ -4,7 +4,6 @@ from autograd import graph
 
 #=nb
 
-
 class Layer_Dense:
     def __init__(self, n_inputs : int, n_neurons : int):
 
@@ -46,16 +45,39 @@ class Activation_Linear:
         return self.output
     def backward(self):
         return self.output.backward()
+class Loss_MSE:
+    def forward(self, inputs, target): #inputs represents the output of the previous layer and the target is the label of the dataset
+        self.inputs = inputs if isinstance(inputs, graph) else graph(inputs)
+        self.target = target if isinstance(target, graph) else graph(target)
+        sub = self.target - self.inputs
+        sqr = sub**2
+        sum = sqr.sum()
+        div = sum /self.target.value.shape[1]
+        self.output = div
+        return self.output
+    def backward(self):
+        return self.output.backward()
+
 
 x = np.random.randn(10, 27*27)
 l1 = Layer_Dense(27*27, 200)
 a1 = Activation_ReLU()
 l2 = Layer_Dense(200, 10)
 a2 = Activation_softmax()
+loss = Loss_MSE()
+
 
 l1.forward(x)
 a1.forward(l1.output)
 l2.forward(a1.output)
 a2.forward(l2.output)
-a2.backward()
-print(l1.weights.value.shape, l1.weights.grad.shape)
+target = graph(np.random.randint(1, 11, size = (10,1)))
+loss.forward(a2.output, target)
+loss.backward()
+print(l1.weights.grad[0][0], l1.weights.value[0][0])
+'''sub = target - a2.output
+sqr = sub**2
+sum = sqr.sum()
+div = sum / 4
+div.backward()
+print(l1.weights.grad.shape, l1.weights.value.shape)'''
