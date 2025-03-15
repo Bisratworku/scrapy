cce083c239948a60cea9d2125a54fb9065c0b4a2
Bisratworku <101055036+Bisratworku@users.noreply.github.com>
2025-03-15 09:49:04 -0700

    increase layer sizes and update training loop for improved performance and visualization

--- scrapy/nn.py
+++ scrapy/nn.py
@@ -91,19 +91,25 @@ class Optimizer_SGD:
 #img,target = idx2numpy.convert_from_file("C:\\Users\\pro\\Documents\\GitHub\\scrapy\\dataset\\train-images.idx3-ubyte"), idx2numpy.convert_from_file("C:\\Users\\pro\\Documents\\GitHub\\scrapy\\dataset\\train-labels.idx1-ubyte")
 X, y = spiral_data(samples=100, classes=3)
 
-l1 = Layer_Dense(2, 100)
+l1 = Layer_Dense(2, 200)
 a1 = Activation_ReLU()
-l2 = Layer_Dense(100,3)
+l2 = Layer_Dense(200,3)
 a2 = Activation_softmax()
 loss = Loss_Catagorical()
-optim = Optimizer_SGD(0.1, 2)
-for i in range(10):
+optim = Optimizer_SGD(0.1, 12)
+
+los = []
+for i in range(100):
     l1.forward(X.reshape(-1, 2))
     a1.forward(l1.output)
     l2.forward(a1.output)
     a2.forward(l2.output)
     loss.forward(a2.output,y)
     print(loss.output)
+    los.append(loss.output.value)
     loss.backward()
-    optim.step(l1)
     optim.step(l2)
+    optim.step(l1)
+u = [1,2,3,4]
+plt.plot(range(len(los)),los)
+plt.show()
