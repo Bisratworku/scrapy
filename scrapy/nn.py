f268addb8543b373cf61f1d1dd2b1128c203b417
Bisratworku <101055036+Bisratworku@users.noreply.github.com>
2025-03-20 10:48:20 -0700

    added adam

--- scrapy/nn.py
+++ scrapy/nn.py
@@ -94,7 +94,7 @@ class Optimizer_SGD:
         layer.weights -= layer.momentum_weights * self.update()
         layer.biases -=  layer.momentum_biases * self.update() 
 class Optimizer_RMSprop:
-    def __init__(self, learning_rate, decay, momentum = 0.9, epsilon = 1e-8):
+    def __init__(self, learning_rate, decay = 0.0 , momentum = 1e-7, epsilon = 1e-8):
         self.learning_rate = learning_rate
         self.decay = decay
         self.current_lr = 0
@@ -111,9 +111,39 @@ class Optimizer_RMSprop:
             layer.momentum_biases =  np.zeros_like(layer.biases.value)
         layer.momentum_weights = self.momentum * layer.momentum_weights + (1 - self.momentum) * layer.weights.grad**2 
         layer.momentum_biases = self.momentum * layer.momentum_biases + (1 - self.momentum) * layer.biases.grad**2
-        layer.weights -=  (layer.weights.grad / self.epsilon + np.sqrt(layer.momentum_weights)) * self.update()
-        layer.biases -=  (layer.biases.grad / self.epsilon + np.sqrt(layer.momentum_biases)) * self.update()
-
+        layer.weights -=  (layer.weights.grad / (self.epsilon + np.sqrt(layer.momentum_weights))) * self.update()
+        layer.biases -=  (layer.biases.grad / (self.epsilon + np.sqrt(layer.momentum_biases))) * self.update()
+class Optimizer_ADAM: # in adam optimization we are combining momenentum with RMS prop 
+    def __init__(self, learning_rate = 1e-3 , decay = 0.0 ,beta_1 = 0.9 , beta_2 = 0.999, epsilon = 1e-8):
+        self.learning_rate = learning_rate
+        self.decay = decay
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+        self.epsilon = epsilon
+        self.epoch = 0
+    def update(self):
+        self.epoch += 1 
+        self.current_lr = (1/(1+self.decay * self.epoch)) * self.learning_rate
+        return self.current_lr    
+    def step(self, layer):
+        if not hasattr(layer, "momentum_weights"):
+            layer.momentum_weights = np.zeros_like(layer.weights.value) # both are for the momentum part 
+            layer.momentum_biases = np.zeros_like(layer.biases.value)
+            layer.iter = 1
+            layer.squared_weights = np.zeros_like(layer.weights.value) # bothe are for RMS part
+            layer.squared_biases = np.zeros_like(layer.biases.value)   
+        layer.momentum_weights = self.beta_1 * layer.momentum_weights + (1 - self.beta_1) * layer.weights.grad
+        corrected_momentum_weights = layer.momentum_weights / (1 + self.beta_1**(layer.iter)) # bias correction 
+        layer.momentum_biases = self.beta_1 * layer.momentum_biases + (1 - self.beta_1) * layer.biases.grad
+        corrected_momentum_biases = layer.momentum_biases / (1 + self.beta_1**(layer.iter)) # bias correction 
+        layer.squared_weights = self.beta_2 * layer.squared_weights + (1 - self.beta_2) * layer.weights.grad**2
+        corrected_squared_weights = layer.squared_weights / (1 + self.beta_2**(layer.iter))
+        layer.squared_biases = self.beta_2 * layer.squared_biases + (1 - self.beta_2) * layer.biases.grad**2
+        corrected_squared_biases =  layer.squared_biases / (1 + self.beta_2**(layer.iter))
+        layer.iter += 1
+        layer.weights -= self.update() * (corrected_momentum_weights / (self.epsilon + np.sqrt(corrected_squared_weights)))  
+        layer.biases -= self.update() * (corrected_momentum_biases / (self.epsilon + np.sqrt(corrected_squared_biases))) 
+        
 
 
 #img,target = idx2numpy.convert_from_file("C:\\Users\\pro\\Documents\\GitHub\\scrapy\\dataset\\train-images.idx3-ubyte"), idx2numpy.convert_from_file("C:\\Users\\pro\\Documents\\GitHub\\scrapy\\dataset\\train-labels.idx1-ubyte")
@@ -125,18 +155,14 @@ a1 = Activation_ReLU()
 l2 = Layer_Dense(200,3)
 a2 = Activation_softmax()
 loss = Loss_Catagorical()
-optim = Optimizer_RMSprop(0.001, 0.95)
+optim = Optimizer_ADAM()
 
-
-for i in range(100):
-    l1.forward(X.reshape(-1, 2))
+for i in range(10):
+    l1.forward(X.reshape(-1,2))
     a1.forward(l1.output)
     l2.forward(a1.output)
     a2.forward(l2.output)
     loss.forward(a2.output,y)
-    print(loss.output)
-    loss.backward()
+    print(loss.output.value)
     optim.step(l2)
     optim.step(l1)
-    
-
